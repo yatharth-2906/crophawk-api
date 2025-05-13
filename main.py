a15ecebd570dc28abf535132bbe2345cbf0fddb7
Yatharth <97800277+yatharth-2906@users.noreply.github.com>
2025-05-13 05:35:22 +0530

    Updated Routes

--- main.py
+++ main.py
@@ -197,9 +197,6 @@ async def yield_prediction(input_parameters: yield_model_input):
     except Exception as e:
         return {"status": "error", "message": f"Internal Server Error: {str(e)}"}
 
-@app.get("/health")
-def health_check():
-    return {"status": "ok"}
 
 if __name__ == "__main__":
     nest_asyncio.apply()
